3861157788f9fab301cd5a5f1d96d375aa1404d0
Kevin Kim <kevinfkim@gmail.com>
2020-05-26 01:27:57 +0000

    linted. 'solved' for asserting arrays. assert each separately.

--- tail.js
+++ tail.js
@@ -9,11 +9,14 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(arr) {
   let arrTail = [];
-  for (let i = 1; i < arr.length; i++) {
-    arrTail.push(arr[i]);
+  if (arr.length > 0) {
+    return arrTail;
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      arrTail.push(arr[i]);
+    }
+    return arrTail;
   }
-
-  return arrTail;
 };
 
 // Test Code
@@ -24,10 +27,25 @@ assertEqual("kevin", "kim");
 assertEqual(13, 13);
 assertEqual(13, 13.1);
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// Test Case 1
+// const result =  tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-// Test Case: Check the original array 
+// Test Case 1b: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 3
+const single = ["single"];
+const empty = [];
+console.log(tail(single)); // should return an empty array
+console.log(tail(empty)); // should return an empty array
+assertEqual(tail(single).length, 0); // should return pass
+assertEqual(tail(empty).length, 0); // should return pass
