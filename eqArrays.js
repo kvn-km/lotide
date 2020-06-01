c9fa30514a128cd75a52a15aa38cc7e802e33015
Kevin Kim <kevinfkim@gmail.com>
2020-06-01 19:41:25 +0000

    refactored using module.exports.

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`❌ Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-// note to self: starting to try to use arrow functions
 const eqArrays = (arr1, arr2) => {
   let equality = true;
   for (let i = 0; i < arr1.length; i++) {
@@ -21,12 +11,4 @@ const eqArrays = (arr1, arr2) => {
   return equality;
 };
 
-// testing code below:
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
