7f99b7f7ba2335187b6220d44609fd51d27448a4
Kevin Kim <kevinfkim@gmail.com>
2020-06-01 21:00:29 +0000

    making sure everything's working correctly.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
+// const assertEqual = require("../assertEqual");
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -4,3 +4,10 @@ const assertArraysEqual = require("../assertArraysEqual");
 
 // test code
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
