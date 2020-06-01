79104f2e312f3e6e12f7e2bafa530d86061a2199
Kevin Kim <kevinfkim@gmail.com>
2020-06-01 19:35:31 +0000

    refactored using module.exports.

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// Function Implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`❌ Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
 const tail = function(arr) {
   let arrTail = [];
   if (arr.length > 0) {
@@ -19,33 +10,4 @@ const tail = function(arr) {
   }
 };
 
-// Test Code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("kevin", "kevin");
-assertEqual("kevin", "kim");
-assertEqual(13, 13);
-assertEqual(13, 13.1);
-
-// Test Case 1
-// const result =  tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case 1b: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 3
-const single = ["single"];
-const empty = [];
-console.log(tail(single)); // should return an empty array
-console.log(tail(empty)); // should return an empty array
-assertEqual(tail(single).length, 0); // should return pass
-assertEqual(tail(empty).length, 0); // should return pass
+module.exports = tail;
